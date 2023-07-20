c560e99bff07d5858687d0f32ed7a772bb4d1cf8
dday33480 <daviddufau@hotmail.co.uk>
2023-07-20 15:28:18 +0200

    Added fake profiles on About Us page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class MainController extends AbstractController
 {
@@ -18,10 +20,14 @@ class MainController extends AbstractController
     /**
      * @Route("/about-us", name="about_us")
      */
-    public function aboutUs() 
+    public function aboutUs(Request $request, SerializerInterface $serializer) 
     {
+        $jsonData = file_get_contents('../team.json');
+        
+        $members = json_decode($jsonData, true);
+
         return $this->render('main/aboutUs.html.twig',[
-            
+            'teamMembers' => $members
         ]);
     }
 }
