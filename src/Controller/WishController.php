3390df9401d8e2e6e08e75e0b9b5ee4938bd1c98
dday33480 <daviddufau@hotmail.co.uk>
2023-07-21 16:58:40 +0200

    create functionality with request handling

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Form\WishType;
 use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+/**
+ * @Route("/wishes", name="wish_")
+ */
 
 class WishController extends AbstractController
 {
     /**
-     * @Route("/list", name="wish_list")
+     * @Route("/", name="list")
      */
     public function list(WishRepository $wishRepo): Response
     {
@@ -25,7 +31,7 @@ class WishController extends AbstractController
     }
 
     /**
-     *  @Route("/details/{id}", name="list_details")
+     * @Route("/details/{id}", name="details")
      */
     public function details(int $id, WishRepository $wishRepo): Response
     {
@@ -34,5 +40,32 @@ class WishController extends AbstractController
         return $this->render('wish/details.html.twig',[
             'wish' => $wishes
         ]);
+    }
+
+/**
+ * @Route("/create", name="create")
+ */
+    public function create(Request $request, EntityManagerInterface $em)
+    {
+        $wish = new Wish();
+        $wish->setDateCreated(new \DateTime());
+        $wish->setIsPublished(1);
+        $wishCreateForm = $this->createForm(WishType::class, $wish);
+
+        $wishCreateForm->handleRequest($request);
+
+        if ($wishCreateForm->isSubmitted() && $wishCreateForm->isValid())
+        {
+            $em->persist($wish);
+            $em->flush();
+
+            $this->addFlash('success', 'New wish added to Bucket list');
+            return $this->redirectToRoute('wish_list', ['id' => $wish->getId()]);
+        }
+
+        return $this->render('wish/create.html.twig', [
+            'wishForm' => $wishCreateForm->createView()
+        ]);
+        
     }
 }
